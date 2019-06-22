eaf791b971f3e020ebfdd3c3eed56b23b8d59fe3
SHI WEI <250110242@qq.com>
2019-06-22 14:17:40 +0800

    day 6

---
+++ pythonlearning/ReturnFunction.py
@@ -0,0 +1,6 @@
+# @Author: Shi Wei <Minsk>
+# @Date:   2019-05-20
+# @Email:  250110242@qq.com
+# @Project: Demo
+# @Last modified by:   Minsk
+# @Last modified time: 2019-06-22
--- pythonlearning/day5/craps.py
+++ pythonlearning/day5/craps.py
@@ -1,3 +1,4 @@
+"""
 # @Author: Shi Wei <Minsk>
 # @Date:   2019-06-21
 # @Email:  250110242@qq.com
@@ -9,4 +10,48 @@
 #           否则游戏继续 玩家继续摇色子
 #           玩家进入游戏时有1000元的赌注 全部输光游戏结束
 # @Last modified by:   Minsk
-# @Last modified time: 2019-06-21
+# @Last modified time: 2019-06-22
+"""
+from random import randint
+
+#赌资
+money = 1000
+
+#摇色子
+def shake_dice():
+    num = randint(1,6) + randint(1,6)
+    print('抛出结果为%d' % num)
+    return num
+
+while money > 0:
+    print('现在你的赌注为：%d元' % money)
+    needs_go_on = False
+
+    while True:
+        bet = int(input('请下注：'))
+        if bet > 0 and bet <= money:
+            break
+    first_result = shake_dice()
+    #玩家赢(第一次)
+    if first_result == 7 or first_result == 11:
+        print('玩家胜')
+        money += bet
+    #庄家赢(第一次)
+    elif first_result == 2 or first_result == 3 or first_result == 12:
+        print('庄家胜')
+        money -= bet
+    else:
+        needs_go_on = True
+
+    while needs_go_on:
+        current_result = shake_dice()
+        if current_result == 7:
+            print('庄家胜')
+            money -= bet
+            needs_go_on = False
+        elif current_result == first_result:
+            print('玩家胜')
+            money += bet
+            needs_go_on = False
+            
+print('全部输光游戏结束')
